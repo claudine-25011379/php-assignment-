5f2f3693040283d4f814f107e88b1f03d1d77ccc
claudine <uclaudine2017@gmail.com>
2025-04-14 09:50:27 -0700

    initial commit of php assignment

---
+++ check.php
@@ -0,0 +1,3 @@
+<?php
+echo("Checking PHP setup...\n");
+?>
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Uwimana Claudine PHP</title>
+</head>
+<body>
+    <h1>Uwimana Claudine PHP</h1>
+
+    <pre>
+    U   U
+    U   U
+    U   U
+    U   U
+    UUUUU
+    </pre>
+
+    <p>
+        SHA256 hash of "Uwimana Claudine":<br>
+        <?php
+            print hash('sha256', 'Uwimana Claudine');
+        ?>
+    </p>
+</body>
+</html>
